80742f60a0269a9b2d3ac91be721d96a8a718393
Gabor Szabo <gabor@szabgab.com>
2025-11-08 11:02:45 +0200

    enable examples

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ fn main() {
     get_github_com_no_slash_at_the_end();
     get_github_com_link_to_a_file();
     https_gitlab_com();
+    converting_to_lowercase_gitlab_com();
+
+    check_good_url();
+    check_missing_url();
+
+    clone_missing_repo();
+    clone_this_repo();
 }
 
 fn get_github_com_no_slash_at_the_end() {
@@ -56,20 +63,22 @@ fn https_gitlab_com() {
     );
 }
 
-//    // test converting to lowercase  gitlab.com
-//    let repo = Repository::from_url("https://gitlab.com/Szabgab/Rust-digger/").unwrap();
-//    assert_eq!(
-//        repo,
-//        Repository::new("gitlab.com", "szabgab", "rust-digger")
-//    );
-//    assert_eq!(repo.url(), "https://gitlab.com/szabgab/rust-digger");
-//    assert_eq!(repo.owner, "szabgab");
-//    assert_eq!(repo.repo, "rust-digger");
-//    assert_eq!(
-//        repo.path(root).to_str(),
-//        Some("/tmp/gitlab.com/szabgab/rust-digger")
-//    );
-//
+fn converting_to_lowercase_gitlab_com() {
+    let root = Path::new("/tmp");
+    let repo = Repository::from_url("https://gitlab.com/Szabgab/Rust-digger/").unwrap();
+    assert_eq!(
+        repo,
+        Repository::new("gitlab.com", "szabgab", "rust-digger")
+    );
+    assert_eq!(repo.url(), "https://gitlab.com/szabgab/rust-digger");
+    assert_eq!(repo.get_owner(), "szabgab");
+    //    assert_eq!(repo.repo, "rust-digger");
+    assert_eq!(
+        repo.path(root).to_str(),
+        Some("/tmp/gitlab.com/szabgab/rust-digger")
+    );
+}
+
 //    // test salsa
 //    let repo = Repository::from_url("https://salsa.debian.org/szabgab/rust-digger/").unwrap();
 //    assert_eq!(
@@ -94,32 +103,32 @@ fn https_gitlab_com() {
 //        "No match for repo in 'https://blabla.com/'"
 //    );
 
-// fn test_check_good_url() {
-//     let repo = Repository::from_url("https://github.com/szabgab/git-digger").unwrap();
-//     assert!(repo.check_url());
-// }
-//
-// fn test_check_missing_url() {
-//     let repo = Repository::from_url("https://github.com/szabgab/no-such-repo").unwrap();
-//     assert!(!repo.check_url());
-// }
+fn check_good_url() {
+    let repo = Repository::from_url("https://github.com/szabgab/git-digger").unwrap();
+    assert!(repo.check_url());
+}
 
-//fn test_clone_missing_repo() {
-//    let temp_folder = tempfile::tempdir().unwrap();
-//    let repo = Repository::from_url("https://github.com/szabgab/no-such-repo").unwrap();
-//    repo.update_repository(Path::new(temp_folder.path()), true)
-//        .unwrap();
-//    let owner_path = temp_folder.path().join("github.com").join("szabgab");
-//    assert!(owner_path.exists());
-//    assert!(!owner_path.join("no-such-repo").exists());
-//}
-//
-//fn test_clone_this_repo() {
-//    let temp_folder = tempfile::tempdir().unwrap();
-//    let repo = Repository::from_url("https://github.com/szabgab/git-digger").unwrap();
-//    repo.update_repository(Path::new(temp_folder.path()), true)
-//        .unwrap();
-//    let owner_path = temp_folder.path().join("github.com").join("szabgab");
-//    assert!(owner_path.exists());
-//    assert!(owner_path.join("git-digger").exists());
-//}
+fn check_missing_url() {
+    let repo = Repository::from_url("https://github.com/szabgab/no-such-repo").unwrap();
+    assert!(!repo.check_url());
+}
+
+fn clone_missing_repo() {
+    let temp_folder = tempfile::tempdir().unwrap();
+    let repo = Repository::from_url("https://github.com/szabgab/no-such-repo").unwrap();
+    repo.update_repository(Path::new(temp_folder.path()), true)
+        .unwrap();
+    let owner_path = temp_folder.path().join("github.com").join("szabgab");
+    assert!(owner_path.exists());
+    assert!(!owner_path.join("no-such-repo").exists());
+}
+
+fn clone_this_repo() {
+    let temp_folder = tempfile::tempdir().unwrap();
+    let repo = Repository::from_url("https://github.com/szabgab/git-digger").unwrap();
+    repo.update_repository(Path::new(temp_folder.path()), true)
+        .unwrap();
+    let owner_path = temp_folder.path().join("github.com").join("szabgab");
+    assert!(owner_path.exists());
+    assert!(owner_path.join("git-digger").exists());
+}
